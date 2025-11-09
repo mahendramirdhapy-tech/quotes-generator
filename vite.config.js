085015e4ace557a105380bbbd740880a902c7c00
mahendramirdhapy-tech <mahendramirdhapy@gmail.com>
2025-11-09 18:54:57 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,11 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/quotes-generator/',
   build: {
-    outDir: 'dist'
+    outDir: 'dist',
+    sourcemap: false
+  },
+  server: {
+    port: 3000
   }
 })
